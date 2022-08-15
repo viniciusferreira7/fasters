8b323304e6c8d8d4566969841a5d12cde3c43c6e
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-08-15 17:12:19 -0300

    feat(eventcard): aplicado a função de deletar com reducer

--- src/components/EventCard/index.tsx
+++ src/components/EventCard/index.tsx
@@ -1,5 +1,9 @@
+import { type } from 'os'
+import { useContext } from 'react'
 import { AiFillCloseCircle } from 'react-icons/ai'
 import { BsClock } from 'react-icons/bs'
+import { Context } from '../../contexts/EventsProvider'
+import { Types } from '../../contexts/reducer'
 
 import { EventCardContainer, TimeIconContainer } from './styles'
 
@@ -15,9 +19,15 @@ interface EventProps {
 }
 
 export default function EventCard({ event }: EventProps) {
+  const { dispatch } = useContext(Context)
+
+  function handleCloseEvent() {
+    dispatch({ type: Types.Delete, payload: { id: event.id } })
+  }
+
   return (
     <EventCardContainer>
-      <AiFillCloseCircle size={18} />
+      <AiFillCloseCircle size={18} onClick={handleCloseEvent} />
       <h4>{event.title}</h4>
       <p>{event.date}</p>
       <TimeIconContainer>
