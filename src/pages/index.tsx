2e0072e0f9bb3e7773be8a7d80929986f489772d
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-08-17 01:08:23 -0300

    style(home): inserção do icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <>
       <Head>
+        <link rel="icon" href="https://www.fasters.com.br/icon.png" />
         <title>Fasters</title>
       </Head>
       <FastersGrid />
