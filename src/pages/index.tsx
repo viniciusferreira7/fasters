836d0a6312d7b73173c8c1ea766728e2f67c856a
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-08-17 01:34:49 -0300

    feat(global): npm run build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,11 @@ export default function Home() {
   return (
     <>
       <Head>
+        <meta
+          name="description"
+          content="Site onde e possível adicionar uma evento na agenda. "
+        />
+        <meta name="keywords" content="Site, Agenda, Fasters. " />
         <link rel="icon" href="https://www.fasters.com.br/icon.png" />
         <title>Fasters</title>
       </Head>
